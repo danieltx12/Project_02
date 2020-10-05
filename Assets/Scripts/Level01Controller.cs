87de1d5f97e7ccf19e67bd27c86c2ae45f7f0ad3
Daniel <daniel@kerber-family.net>
2020-10-05 15:23:21 -0500

    Start Menu/Return to Menu

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Level01Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            ExitLevel();
+        }
+    }
+
+    private void ExitLevel()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
