5ec8337ae8833fda8dca155b460419f71658ab1e
Daniel <daniel@kerber-family.net>
2020-10-05 17:13:13 -0500

    02A Complete

--- Assets/Scripts/Level01Controller.cs
+++ Assets/Scripts/Level01Controller.cs
@@ -2,19 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Level01Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Text _currentScoreTextView;
+    [SerializeField] GameObject _menu;
+    bool menuState = false;
+    bool cursorState = true;
+    int _currentScore;
+
+     void Start()
+    {
+       Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            ExitLevel();
+           CursorFix();
+            
         }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            {
+            IncreaseScore(5);
+            }
     }
 
-    private void ExitLevel()
+    public void ExitLevel()
     {
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        if (_currentScore > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            Debug.Log("New high score: " + _currentScore);
+        }
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void IncreaseScore(int scoreIncrease)
+    {
+        _currentScore += scoreIncrease;
+        _currentScoreTextView.text = "Score: " + _currentScore.ToString();
+    }
+
+    public void CursorFix()
+    {
+        menuState = !menuState;
+        _menu.SetActive(menuState);
+           
+        Cursor.visible = !cursorState;
+        cursorState = !cursorState;
+        if(Cursor.lockState == CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else if (Cursor.lockState != CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        Debug.Log("TOGGLE");
     }
 }
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] AudioClip _startingSong;
+    [SerializeField] Text _highScoreTextView;
+     void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+        if(_startingSong != null && AudioManager.Instance == null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+
+
+    public void CloseApp()
+    {
+        Debug.Log("Quit App");
+        Application.Quit();
+
+    }
+
+    public void resetData()
+    {
+        PlayerPrefs.SetInt("HighScore", 0);
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+    }
+}
